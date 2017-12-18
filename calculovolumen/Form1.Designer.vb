9c2a4a950ecdeaca4c068038736f2a92812693a3
42394109chr <42394109@continental.edu.pe>
2017-12-17 23:41:35 -0500

    Commit message

---
+++ calculovolumen/Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtrvolesf = New System.Windows.Forms.TextBox()
+        Me.txtradioesf = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.txtalturacil = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btncalcular1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtvolcil = New System.Windows.Forms.TextBox()
+        Me.txtradiocil = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(524, 277)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.btncalcular)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.txtrvolesf)
+        Me.TabPage1.Controls.Add(Me.txtradioesf)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(516, 251)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Volumen de una esfera"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncalcular.Location = New System.Drawing.Point(208, 94)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 25)
+        Me.btncalcular.TabIndex = 5
+        Me.btncalcular.Text = "Calcular"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(39, 137)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(244, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "El volummen de la esfera es :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(149, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(162, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Volumen de Esfera"
+        '
+        'txtrvolesf
+        '
+        Me.txtrvolesf.Location = New System.Drawing.Point(292, 139)
+        Me.txtrvolesf.Name = "txtrvolesf"
+        Me.txtrvolesf.Size = New System.Drawing.Size(135, 20)
+        Me.txtrvolesf.TabIndex = 2
+        '
+        'txtradioesf
+        '
+        Me.txtradioesf.Location = New System.Drawing.Point(292, 57)
+        Me.txtradioesf.Name = "txtradioesf"
+        Me.txtradioesf.Size = New System.Drawing.Size(100, 20)
+        Me.txtradioesf.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(149, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese el radio"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.txtalturacil)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.btncalcular1)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.txtvolcil)
+        Me.TabPage2.Controls.Add(Me.txtradiocil)
+        Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.Label4)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(516, 251)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Volumen de cilindro"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'txtalturacil
+        '
+        Me.txtalturacil.Location = New System.Drawing.Point(304, 101)
+        Me.txtalturacil.Name = "txtalturacil"
+        Me.txtalturacil.Size = New System.Drawing.Size(100, 20)
+        Me.txtalturacil.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(174, 101)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(121, 20)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Ingrese altura"
+        '
+        'btncalcular1
+        '
+        Me.btncalcular1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncalcular1.Location = New System.Drawing.Point(220, 148)
+        Me.btncalcular1.Name = "btncalcular1"
+        Me.btncalcular1.Size = New System.Drawing.Size(75, 25)
+        Me.btncalcular1.TabIndex = 10
+        Me.btncalcular1.Text = "Calcular"
+        Me.btncalcular1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(51, 191)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(244, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "El volummen de la esfera es :"
+        '
+        'txtvolcil
+        '
+        Me.txtvolcil.Location = New System.Drawing.Point(304, 193)
+        Me.txtvolcil.Name = "txtvolcil"
+        Me.txtvolcil.Size = New System.Drawing.Size(135, 20)
+        Me.txtvolcil.TabIndex = 8
+        '
+        'txtradiocil
+        '
+        Me.txtradiocil.Location = New System.Drawing.Point(304, 60)
+        Me.txtradiocil.Name = "txtradiocil"
+        Me.txtradiocil.Size = New System.Drawing.Size(100, 20)
+        Me.txtradiocil.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(161, 60)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(134, 20)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Ingrese el radio"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(161, 20)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(169, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Volumen de Cilindro"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(524, 277)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Sistema Cálculo de Volumen 1.0."
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents btncalcular As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtrvolesf As System.Windows.Forms.TextBox
+    Friend WithEvents txtradioesf As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtalturacil As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btncalcular1 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtvolcil As System.Windows.Forms.TextBox
+    Friend WithEvents txtradiocil As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ calculovolumen/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+    Public pi As Double
+
+    Private Sub btncalcular_Click(sender As System.Object, e As System.EventArgs) Handles btncalcular.Click
+
+        Try
+
+
+            Dim radesfera As Double
+            Dim volesfera As Double
+
+            pi = 3.14
+            radesfera = CType(txtradioesf.Text, Double)
+
+            volesfera = (4 / 3) * (pi) * (Math.Pow(radesfera, 3))
+
+            txtrvolesf.Text = volesfera
+
+        Catch ex As Exception
+
+            MsgBox("Ingrese los datos correctos", MsgBoxStyle.Critical)
+
+        End Try
+
+
+    End Sub
+
+ 
+   
+    Private Sub btncalcular1_Click(sender As System.Object, e As System.EventArgs) Handles btncalcular1.Click
+
+        Try
+
+            Dim radcilindro As Double
+            Dim alturacilindro As Double
+
+            Dim volcilindro As Double
+
+            pi = 3.14
+            radcilindro = CType(txtradiocil.Text, Double)
+
+            alturacilindro = CType(txtalturacil.Text, Double)
+
+            volcilindro = (pi) * (Math.Pow(radcilindro, 2)) * alturacilindro
+
+            txtvolcil.Text = volcilindro
+
+
+        Catch ex As Exception
+
+            MsgBox("Ingrese los datos correctos", MsgBoxStyle.Critical)
+
+        End Try
+
+    End Sub
+
+
+End Class
